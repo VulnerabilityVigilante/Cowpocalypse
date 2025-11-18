c79cd3033f27a7cf541d38d908da3919b009a635
VulnerabilityVigilante <122943237+VulnerabilityVigilante@users.noreply.github.com>
2025-11-18 14:23:57 -0600

    End of quest quest 2 & start of quest 3

--- Assets/Resources/Scripts/ShopUI.cs
+++ Assets/Resources/Scripts/ShopUI.cs
@@ -6,6 +6,9 @@ public class ShopUI : MonoBehaviour
     public GameObject shopPanel;
     public bool ShopIsOpen { get; private set; }
 
+    // 👉 Track what the player has bought during this quest
+    private bool boughtRedbull = false;
+    private bool boughtCigarettes = false;
 
     void Awake()
     {
@@ -16,13 +19,15 @@ public class ShopUI : MonoBehaviour
     {
         int cost = 10;
 
-        // Try spending coins
         bool purchased = CurrencyManager.Instance.SpendCoins(cost);
 
         if (purchased)
         {
             Debug.Log("You bought a Redbull!");
             // TODO: Add to inventory when ready
+
+            boughtRedbull = true;
+            CheckSpendAtShopQuestProgress();
         }
         else
         {
@@ -34,13 +39,15 @@ public class ShopUI : MonoBehaviour
     {
         int cost = 30;
 
-        // Try spending coins
         bool purchased = CurrencyManager.Instance.SpendCoins(cost);
 
         if (purchased)
         {
             Debug.Log("You bought a pack of cigarettes!");
             // TODO: Add to inventory when ready
+
+            boughtCigarettes = true;
+            CheckSpendAtShopQuestProgress();
         }
         else
         {
@@ -48,6 +55,24 @@ public class ShopUI : MonoBehaviour
         }
     }
 
+    // 👉 Call this after each successful purchase
+    private void CheckSpendAtShopQuestProgress()
+    {
+        // Make sure the ID string EXACTLY matches the one in QuestManager/QuestGiver
+        string questId = "SpendAtShopQuest";
+
+        if (QuestManager.Instance == null) return;
+
+        // Only care if the quest is active
+        if (QuestManager.Instance.GetQuestState(questId) == QuestState.InProgress)
+        {
+            if (boughtRedbull && boughtCigarettes)
+            {
+                Debug.Log("[ShopUI] Both items bought, completing SpendAtShopQuest.");
+                QuestManager.Instance.CompleteQuest(questId);
+            }
+        }
+    }
 
     public void Open()
     {
@@ -66,8 +91,4 @@ public class ShopUI : MonoBehaviour
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
-
-
-
-
 }
